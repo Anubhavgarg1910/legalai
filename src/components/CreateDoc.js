22eafc6f3016e1a06e6a235c86ee04dee57e869f
Aviral <aviral2002@gmail.com>
2024-05-12 16:32:20 +0530

    till form

--- src/components/CreateDoc.js
+++ src/components/CreateDoc.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Banner from "./Banner";
-import RentalAgreement from "./RentalAgreement";
+import rentalImg from "../assets/img/rental image.jpg"
 
 const FormKeys = {
   date: "date",
@@ -13,134 +13,137 @@ const FormKeys = {
 };
 
 const CreateDoc = () => {
-  const [openStates, setOpenStates] = useState({
-    rentalOpen: false,
-    loanOpen: false,
-    businessOpen: false,
-    employmentOpen: false,
-  });
-  const [openTemplate, setOpenTemplate] = useState({
-    rentalForm: false,
-    loanForm: false,
-    businessForm: false,
-    employmentForm: false,
-  });
-
-  const [rentalData, setRentalData] = useState({});
+  const [file, setFile] = useState(null);
+  const [summary, setSummary] = useState("");
+  const [rentalOpen, setRentalOpen] = useState(false);
+  const [myTemplate, setMyTemplate] = useState(false);
+  const [formData, setFormData] = useState({});
 
   const onRentalAgreement = () => {
-    setOpenStates((prevState) => ({
-      ...prevState,
-      rentalOpen: true,
-    }));
+    setRentalOpen(true);
   };
 
   const setData = (key, val) => {
-    const tempData = { ...rentalData };
+    const tempData = { ...formData };
     tempData[key] = val;
-    setRentalData(tempData);
+    setFormData(tempData);
   };
   const handleSubmit = () => {
-    if (Object.keys(rentalData).length == 0) {
-      setOpenStates((prevState) => ({
-        ...prevState,
-        rentalOpen: true,
-      }));
-    } else {
-      setOpenStates((prevState) => ({
-        ...prevState,
-        rentalOpen: false,
-      }));
-    }
-    setOpenTemplate((prevState) => ({
-      ...prevState,
-      rentalForm: true,
-    }));
+    console.log(formData);
+    setRentalOpen(false);
+    setMyTemplate(true);
   };
 
   return (
+   
     <div>
       <Banner showAppointment={false} />
       <div>
-        <div
-          style={{
-            backgroundColor: "red",
-            width: "100%",
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "5%",
-          }}
-        >
-          <button onClick={onRentalAgreement}>Rental Agreement</button>
-          {openStates?.rentalOpen && (
-            <div>
+        <div className="flex flex-col items-center mt-[100px]  mb-16 last:mb-0 lg:mb-0 justify-center ">
+          <button className='text-2xl mb-2 font-primary font-bold ' onClick={onRentalAgreement}>Rental Agreement</button>
+          <p className=' max-w-[332px] lg:max-w-[350px] text-center'>Make home,shop etc. rental agreement in seconds by just filling a form</p>
+
+          <button className='text-2xl mb-[20px] font-primary font-bold mt-[50px]'>Loan Agreement</button>
+          <p className=' max-w-[332px] lg:max-w-[350px] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum</p>
+
+          <button className='text-2xl mb-2 font-primary font-bold mt-[50px]'>Employment Agreement</button>
+           <p className=' max-w-[332px] lg:max-w-[350px] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum</p>
+           
+          <button className='text-2xl mb-2 font-primary font-bold mt-[50px]'>Business Agreement</button>
+          <p className=' max-w-[332px] lg:max-w-[350px] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum</p>
+          </div>
+
+
+          <div className=" flex flex-row justify-center mt-[200px]">
+          {rentalOpen && (
+            <div className="  bg-primary w-full p-[74px] rounded-sm  ">
               <div>
-                <span>Date</span>
-                <input
-                  value={rentalData[FormKeys.date]}
+               
+                <input className="form-control" placeholder="Date"
+                  value={formData[FormKeys.date]}
                   onChange={(e) => {
                     setData([FormKeys.date], e.target.value);
                   }}
                 />
               </div>
               <div>
-                <span>LandLord Name</span>
-                <input
-                  value={rentalData[FormKeys.landlordName]}
+                
+                <input className="form-control"
+                placeholder="LandLord Name"
+                  value={formData[FormKeys.landlordName]}
                   onChange={(e) => {
                     setData([FormKeys.landlordName], e.target.value);
                   }}
                 />
               </div>
               <div>
-                <span>Landlord Address</span>
-                <input
-                  value={rentalData[FormKeys.landlordAddress]}
+                
+                <input className="form-control" placeholder="Landlord Address"
+                  value={formData[FormKeys.landlordAddress]}
                   onChange={(e) => {
                     setData([FormKeys.landlordAddress], e.target.value);
                   }}
                 />
               </div>
               <div>
-                <span>Tenant Name</span>
-                <input
-                  value={rentalData[FormKeys.tenantName]}
+                
+                <input className="form-control" placeholder="Tenant Name"
+                  value={formData[FormKeys.tenantName]}
                   onChange={(e) => {
                     setData([FormKeys.tenantName], e.target.value);
                   }}
                 />
               </div>
               <div>
-                <span>Tenant Address</span>
-                <input
-                  value={rentalData[FormKeys.tenantAddress]}
+                
+                <input className="form-control" placeholder="Tenant Address"
+                  value={formData[FormKeys.tenantAddress]}
                   onChange={(e) => {
                     setData([FormKeys.tenantAddress], e.target.value);
                   }}
                 />
               </div>
               <div>
-                <span>Rent</span>
-                <input
-                  value={rentalData[FormKeys.rent]}
+                
+                <input className="form-control" placeholder="Rent"
+                  value={formData[FormKeys.rent]}
                   onChange={(e) => {
                     setData([FormKeys.rent], e.target.value);
                   }}
                 />
               </div>
 
-              <div onClick={handleSubmit}>Submit</div>
+              <button className="btn bg-accent hover:bg-accent-hover transition-all" onClick={handleSubmit}>Submit</button>
             </div>
           )}
-
-          {openTemplate?.rentalForm && rentalData && (
-            <RentalAgreement rentalAgreementData={rentalData} />
+          {myTemplate && formData && (
+            <div className="">
+              This Rental Agreement is made and entered into on {formData?.date}
+              , by and between: Landlord:
+              {formData?.landlordName}, residing at
+              {formData?.landlordAddress} and Tenant: {formData?.tenantName},
+              residing at {formData?.tenantAddress}Property: The Landlord agrees to rent to
+              the Tenant the premises located at {formData?.landlordAddress} for
+              residential purposes only. Term: The term of this Agreement shall
+              begin on {formData?.date} and shall continue on a month-to-month
+              basis until terminated as provided herein. Rent: The Tenant shall
+              pay to the Landlord a monthly rent of {formData?.rent} to be paid
+              on the 5th of each month. Maintenance: The Landlord shall be
+              responsible for maintaining the Property in a habitable condition
+              and shall make any necessary repairs promptly. Termination: Either
+              party may terminate this Agreement upon giving written notice to
+              the other party at least 30 days prior to the intended date of
+              termination. Governing Law: This Agreement shall be governed by
+              and construed in accordance with the laws of India. Entire
+              Agreement: This Agreement constitutes the entire understanding and
+              agreement between the parties with respect to the subject matter
+              hereof and supersedes all prior agreements and understandings. IN
+              WITNESS WHEREOF, the parties hereto have executed this Agreement
+              as of the date first above written.
+            </div>
           )}
-
-          <button>Loan Agreement</button>
-          <button>Employment Agreement</button>
-          <button>Business Agreement</button>
-        </div>
+          </div>
+        
       </div>
     </div>
   );
