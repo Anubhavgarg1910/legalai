5de922baeeafdcb4112fdc708aea1cb454970249
Anubhavgarg1910 <utkarshgarg422@gmail.com>
2024-05-16 20:21:55 +0530

    changes done

--- src/components/CreateDoc.js
+++ src/components/CreateDoc.js
@@ -6,6 +6,7 @@ import { FormKeys } from "../constants/constants";
 import { LoanKeys } from "../constants/constants";
 import { BusinessKeys } from "../constants/constants";
 import { EmpKeys } from "../constants/constants";
+import LoanAgreement from "./LoanAgreement";
 
 const CreateDoc = () => {
   const [rentalOpen, setRentalOpen] = useState(false);
@@ -31,15 +32,15 @@ const CreateDoc = () => {
   };
   const onLoanAgreement = () => {
     setLoanOpen(true);
-    // setMyTemplate(false);
+    setMyTemplate(false);
   };
   const onBusinessAgreement = () => {
     setBusinessOpen(true);
-    // setMyTemplate(false);
+    setMyTemplate(false);
   };
   const onEmployAgreement = () => {
     setEmpOpen(true);
-    // setMyTemplate(false);
+    setMyTemplate(false);
   };
 
   const setData = (key, val) => {
@@ -66,6 +67,12 @@ const CreateDoc = () => {
     console.log(formData);
     setRentalOpen(false);
     setMyTemplate(true);
+  };
+  const handleSubmit2 = () => {
+    setLoanOpen(false);
+    console.log(loanData);
+
+    // setMyTemplate(true);
   };
 
   return (
@@ -119,6 +126,295 @@ const CreateDoc = () => {
           </p>
         </div>
 
+        <div className=" flex flex-row justify-center mb-[100px]">
+          {rentalOpen && (
+            <div className="  bg-primary p-[74px] w-[700px] rounded-3xl">
+              <div className="py-3">
+                <input
+                  className="form-control"
+                  placeholder="Date"
+                  value={formData[FormKeys.date]}
+                  onChange={(e) => {
+                    setData([FormKeys.date], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control"
+                  placeholder="LandLord Name"
+                  value={formData[FormKeys.landlordName]}
+                  onChange={(e) => {
+                    setData([FormKeys.landlordName], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control p-2"
+                  placeholder="Landlord Address"
+                  value={formData[FormKeys.landlordAddress]}
+                  onChange={(e) => {
+                    setData([FormKeys.landlordAddress], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control p-2"
+                  placeholder="Tenant Name"
+                  value={formData[FormKeys.tenantName]}
+                  onChange={(e) => {
+                    setData([FormKeys.tenantName], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Tenant Address"
+                  value={formData[FormKeys.tenantAddress]}
+                  onChange={(e) => {
+                    setData([FormKeys.tenantAddress], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Rent"
+                  value={formData[FormKeys.rent]}
+                  onChange={(e) => {
+                    setData([FormKeys.rent], e.target.value);
+                  }}
+                />
+              </div>
+              <div className=" pt-6">
+                <button
+                  className="btn bg-accent hover:bg-accent-hover transition-all  "
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          )}
+          {myTemplate && formData && (
+            <div className="w-[900px] p-6 border-double border-4 border-black">
+              <RentalAgreement rentalAgreementData={formData} />
+            </div>
+          )}
+        </div>
+        <div className=" flex flex-row justify-center  mb-[100px]">
+          {loanOpen && (
+            <div className="  bg-primary p-[74px] w-[700px] rounded-3xl">
+              <div className="py-3">
+                <input
+                  className="form-control"
+                  placeholder="Date"
+                  value={loanData[LoanKeys.date]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.date], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control"
+                  placeholder="Lender Name"
+                  value={loanData[LoanKeys.lenderName]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.lenderName], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control p-2"
+                  placeholder="Lender Address"
+                  value={loanData[LoanKeys.lenderAddress]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.lenderAddress], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control p-2"
+                  placeholder="Borrower Name"
+                  value={loanData[LoanKeys.borrowerName]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.borrowerName], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Borrower Address"
+                  value={loanData[LoanKeys.borrowerAddress]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.borrowerAddress], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Loan Amount"
+                  value={loanData[LoanKeys.loanAmount]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.loanAmount], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Interest Rate"
+                  value={loanData[LoanKeys.interestRate]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.interestRate], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Start Date"
+                  value={loanData[LoanKeys.startDate]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.startDate], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="End Date"
+                  value={loanData[LoanKeys.endDate]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.endDate], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Installment Amount"
+                  value={loanData[LoanKeys.installmentAmount]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.installmentAmount], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="First Installment Date"
+                  value={loanData[LoanKeys.firstInstallmentDate]}
+                  onChange={(e) => {
+                    setLData([LoanKeys.firstInstallmentDate], e.target.value);
+                  }}
+                />
+              </div>
+
+              <div className=" pt-6">
+                <button
+                  className="btn bg-accent hover:bg-accent-hover transition-all  "
+                  onClick={handleSubmit2}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          )}
+          {myTemplate && loanData && (
+            <div className="w-[900px] p-6 border-double border-4 border-black">
+              <LoanAgreement loanAgreementData={loanData} />
+            </div>
+          )}
+        </div>
+        <div className=" flex flex-row justify-center mt-[200px] mb-[100px]">
+          {rentalOpen && (
+            <div className="  bg-primary p-[74px] w-[700px] rounded-3xl">
+              <div className="py-3">
+                <input
+                  className="form-control"
+                  placeholder="Date"
+                  value={formData[FormKeys.date]}
+                  onChange={(e) => {
+                    setData([FormKeys.date], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control"
+                  placeholder="LandLord Name"
+                  value={formData[FormKeys.landlordName]}
+                  onChange={(e) => {
+                    setData([FormKeys.landlordName], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control p-2"
+                  placeholder="Landlord Address"
+                  value={formData[FormKeys.landlordAddress]}
+                  onChange={(e) => {
+                    setData([FormKeys.landlordAddress], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control p-2"
+                  placeholder="Tenant Name"
+                  value={formData[FormKeys.tenantName]}
+                  onChange={(e) => {
+                    setData([FormKeys.tenantName], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Tenant Address"
+                  value={formData[FormKeys.tenantAddress]}
+                  onChange={(e) => {
+                    setData([FormKeys.tenantAddress], e.target.value);
+                  }}
+                />
+              </div>
+              <div className="py-3">
+                <input
+                  className="form-control "
+                  placeholder="Rent"
+                  value={formData[FormKeys.rent]}
+                  onChange={(e) => {
+                    setData([FormKeys.rent], e.target.value);
+                  }}
+                />
+              </div>
+              <div className=" pt-6">
+                <button
+                  className="btn bg-accent hover:bg-accent-hover transition-all  "
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          )}
+          {myTemplate && formData && (
+            <div className="w-[900px] p-6 border-double border-4 border-black">
+              <RentalAgreement rentalAgreementData={formData} />
+            </div>
+          )}
+          {}
+        </div>
         <div className=" flex flex-row justify-center mt-[200px] mb-[100px]">
           {rentalOpen && (
             <div className="  bg-primary p-[74px] w-[700px] rounded-3xl">
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,31 +8,15 @@ import BackToTopBtn from "./BackToTopBtn";
 import MyFormComponent from "./MyFormComponent";
 
 const HomePage = () => {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:5000/");
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.log({ error });
-    }
-  };
-  console.log({ data });
   return (
     <div>
-      <span>{data?.Date}</span>
-
       <Banner showAppointment={true} />
       <Testimonials />
       <Skills />
       <Contact />
       <Footer />
       <BackToTopBtn />
-      <MyFormComponent/>
+      <MyFormComponent />
     </div>
   );
 };
