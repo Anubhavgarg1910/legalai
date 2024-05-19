ebd7e0367e89b7d7e92043f6fb74a0210912a0ec
Anubhavgarg1910 <utkarshgarg422@gmail.com>
2024-05-19 20:45:06 +0530

    changes format done

--- src/components/BusinessContract.js
+++ src/components/BusinessContract.js
@@ -4,30 +4,49 @@ const BusinessContract = ({ businessContractData }) => {
   return (
     <div>
       This Business Contract is made and entered into on{" "}
-      {businessContractData?.date}, by and between:{" "}
+      {businessContractData?.date}
+      <br />
+      by and between: <br />
       {businessContractData?.partyA}, a {businessContractData?.businessType}
       organized and existing under the laws of India, with its principal place
-      of business located at {businessContractData?.addressPartyA} , and{" "}
-      {businessContractData?.partyB}, a {businessContractData?.businessType}{" "}
-      organized and existing under the laws of India, with its principal place
-      of business located at {businessContractData?.addressPartyB}. 1. Purpose
-      of Contract: The purpose of this Contract is to establish the terms and
-      conditions under which Party A and Party B will engage in business
-      activities together. 2. Scope of Work: {businessContractData?.partyA}{" "}
-      agrees to provide {businessContractData?.businessDesc} to{" "}
-      {businessContractData?.partyB}, and agrees to
-      {businessContractData?.payment} for the services/products provided. 3.
-      Term and Termination: This Contract shall commence on{" "}
+      of business located at {businessContractData?.addressPartyA}
+      <br /> {businessContractData?.partyB}, a{" "}
+      {businessContractData?.businessType} organized and existing under the laws
+      of India, with its principal place of business located at{" "}
+      {businessContractData?.addressPartyB}.
+      <br /> 1. Purpose of Contract: The purpose of this Contract is to
+      establish the terms and conditions under which Party A and Party B will
+      engage in business activities together.
+      <br />
+      2. Scope of Work: {businessContractData?.partyA} agrees to provide{" "}
+      {businessContractData?.businessDesc} to {businessContractData?.partyB},
+      and agrees to
+      {businessContractData?.payment} for the services/products provided.
+      <br />
+      3. Term and Termination: This Contract shall commence on{" "}
       {businessContractData?.date} and shall continue until{" "}
       {businessContractData?.endingDate}. Either party may terminate this
-      Contract upon 2 month's written notice to the other party. 4. Payment
-      Terms: {businessContractData?.partyB} shall pay{" "}
+      Contract upon 2 month's written notice to the other party.
+      <br />
+      4. Payment Terms: {businessContractData?.partyB} shall pay{" "}
       {businessContractData?.partyA} the agreed-upon amount of{" "}
       {businessContractData?.payment} for the services/products provided.
       Payment shall be made and shall be due on {businessContractData?.dueDate}.
+      <br />
       5. Confidentiality: Both parties agree to maintain the confidentiality of
       any proprietary or confidential information disclosed during the term of
       this Contract.
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "2%",
+          marginBottom: "2%",
+        }}
+      >
+        <span>Signature Party A</span>
+        <span>Signature Party B</span>
+      </div>
     </div>
   );
 };
--- src/components/EmployementContract.js
+++ src/components/EmployementContract.js
@@ -4,44 +4,73 @@ const EmployementContract = ({ employementContractData }) => {
   return (
     <div>
       This Employment Contract is made and entered into on{" "}
-      {employementContractData?.date}, by and between: Employer:{" "}
-      {employementContractData?.employerName}
-      Address: {employementContractData?.employerAddress}
-      and Employee: {employementContractData?.employeeName} Address:{" "}
-      {employementContractData?.employeeAddress} Position: The Employer agrees
-      to employ the Employee in the position of{" "}
-      {employementContractData?.jobTitle}. Commencement Date: The employment
-      shall commence on {employementContractData?.startDate}. Term: The initial
-      term of employment shall be for a period of 6 months, beginning on{" "}
-      {employementContractData?.startDate} and ending on
+      {employementContractData?.date}
+      <br />
+      by and between: <br />
+      Employer: {employementContractData?.employerName} residing at{" "}
+      {employementContractData?.employerAddress}
+      <br />
+      Employee: {employementContractData?.employeeName} residing at{" "}
+      {employementContractData?.employeeAddress}
+      <br />
+      Position: The Employer agrees to employ the Employee in the position of{" "}
+      {employementContractData?.jobTitle}.
+      <br />
+      Commencement Date: The employment shall commence on{" "}
+      {employementContractData?.startDate}.
+      <br />
+      Term: The initial term of employment shall be for a period of 6 months,
+      beginning on {employementContractData?.startDate} and ending on
       {employementContractData?.endDate}. The employment may be terminated
-      earlier in accordance with the provisions of this Contract. Salary: The
-      Employee shall be paid a salary of {employementContractData?.salaryAmount}{" "}
-      per month payable {employementContractData?.paymentFreq}
+      earlier in accordance with the provisions of this Contract.
+      <br />
+      Salary: The Employee shall be paid a salary of{" "}
+      {employementContractData?.salaryAmount} per month payable{" "}
+      {employementContractData?.paymentFreq}
       in arrears, subject to applicable deductions and withholdings. Benefits:
       The Employee shall be entitled to {employementContractData?.empBenefits},
       including but not limited to [Health Insurance, Retirement Plan, Paid Time
-      Off, etc.]. Duties and Responsibilities: The Employee shall perform the
-      duties and responsibilities associated with the position of{" "}
+      Off, etc.].
+      <br />
+      Duties and Responsibilities: The Employee shall perform the duties and
+      responsibilities associated with the position of{" "}
       {employementContractData?.jobTitle}, as outlined in the job description
-      provided by the Employer. Confidentiality: During the term of employment
-      and thereafter, the Employee shall not disclose any confidential
-      information belonging to the Employer, including but not limited to trade
-      secrets, customer information, and proprietary business information.
+      provided by the Employer.
+      <br />
+      Confidentiality: During the term of employment and thereafter, the
+      Employee shall not disclose any confidential information belonging to the
+      Employer, including but not limited to trade secrets, customer
+      information, and proprietary business information.
+      <br />
       Non-Compete: The Employee agrees not to engage in any employment,
       consulting, or business activities that compete with the Employer's
       business during the term of employment and for a period of 1 year
-      following the termination of employment. Termination: This Contract may be
-      terminated by either party upon 2 month's written notice to the other
-      party. The Employer reserves the right to terminate the employment for
-      cause, including but not limited to misconduct, poor performance, or
-      violation of company policies. Governing Law: This Contract shall be
-      governed by and construed in accordance with the laws of the state of
-      India. Entire Agreement: This Contract constitutes the entire agreement
-      between the parties and supersedes all prior agreements and
-      understandings, whether written or oral, relating to the subject matter
-      herein. IN WITNESS WHEREOF, the parties hereto have executed this Contract
-      as of the date first above written.
+      following the termination of employment.
+      <br />
+      Termination: This Contract may be terminated by either party upon 2
+      month's written notice to the other party. The Employer reserves the right
+      to terminate the employment for cause, including but not limited to
+      misconduct, poor performance, or violation of company policies.
+      <br />
+      Governing Law: This Contract shall be governed by and construed in
+      accordance with the laws of the state of India. Entire Agreement: This
+      Contract constitutes the entire agreement between the parties and
+      supersedes all prior agreements and understandings, whether written or
+      oral, relating to the subject matter herein. IN WITNESS WHEREOF, the
+      parties hereto have executed this Contract as of the date first above
+      written.
+      <br />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "2%",
+          marginBottom: "2%",
+        }}
+      >
+        <span>Signature Employer</span>
+        <span>Signature Employee</span>
+      </div>
     </div>
   );
 };
